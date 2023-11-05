cf3d96491dcf77f688477bb6353d135a8c78ff44
ParthaMalakar <parthamalakar4321@gmail.com>
2023-11-06 00:56:52 +0600

    menu section created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import TopFood from './TopFood/TopFood';
 import { useLoaderData } from 'react-router-dom';
+import Reservation from './Reservation/Reservation';
+import Menu from './Menu/Menu';
 
 const Home = () => {
     const Foods= useLoaderData();
@@ -10,6 +12,12 @@ const Home = () => {
             
         <Banner ></Banner>
         <TopFood Foods={Foods}></TopFood>
+        
+        <div className='bg-slate-200 pt-3 max-w-7xl mx-auto'>
+        <Reservation></Reservation>
+        
+        </div>
+        <Menu></Menu>
         </div>
     );
 };
