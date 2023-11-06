bdf03993c80c6277a5be54f8240beb793524454f
ParthaMalakar <parthamalakar4321@gmail.com>
2023-11-06 20:32:31 +0600

    registration password validation completed and handleraegistration event handler created sweetalert added

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Routes.jsx'
+import Authprovider from './provider/Authprovider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+ <Authprovider>
  <RouterProvider router={router} />
+ </Authprovider>
   </React.StrictMode>,
 )
--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,12 +1,110 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import {  useNavigate } from "react-router-dom";
+import { AuthContext } from "../../provider/Authprovider";
+import Swal from "sweetalert2";
+import { updateProfile } from "firebase/auth";
 
 
 const Registration = () => {
+    const {createUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+    
+
+
+    const handleRegistration = e => {
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+
+        const name = form.get('name');
+        const photo = form.get('photo');
+        const email = form.get('email');
+        const password = form.get('password');
+        console.log(name, photo, email, password);
+
+        if (password.length < 6) {
+            Swal.fire({
+                title: 'Password should be at least 6 characters or longer!',
+                text: 'Do you want to continue',
+                icon: 'error',
+                confirmButtonText: 'Cool'
+              })
+            return;
+        }
+        else if (!/[A-Z]/.test(password)) {
+    
+            Swal.fire({
+                title: 'Your password should have at least one upper case characters!',
+                text: 'Do you want to continue',
+                icon: 'error',
+                confirmButtonText: 'Cool'
+              })
+
+            return;
+        }
+        else if (!/[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/.test(password)) {
+            
+            Swal.fire({
+                title: 'Password must contain at least one special characters',
+                text: 'Do you want to continue',
+                icon: 'error',
+                confirmButtonText: 'Cool'
+              })
+            return;
+        }
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                const mycart = [];
+                const userdata = { email, MyCart: mycart };
+                fetch('http://localhost:5000/user', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(userdata)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if(data.insertedId){
+                            console.log('user added to the database')
+                        }
+                    })
+
+                updateProfile(user, {
+                    displayName: name , 
+                    photoURL: photo , 
+                  }).then(() => {
+                    console.log("User profile updated");
+                    window.location.reload(true)
+                  }).catch((error) => {
+                    console.error("Error updating profile:", error);
+                  });
+                
+                  Swal.fire(
+                    'Registration Successfully complete',
+                    'You clicked the button!',
+                    'success'
+                  )
+                  e.target.reset();
+                  navigate(location?.state ? location.state : '/');
+                  
+                            })
+            .catch(error => {
+                console.error(error)
+                Swal.fire({
+                    title: `${error.message}`,
+                    text: 'Do you want to continue',
+                    icon: 'error',
+                    confirmButtonText: 'Cool'
+                  })
+            })
+        }
     return (
         <div>
              <div className="bg-[#837b799f]">
                 <h2 className="text-3xl pt-8 text-center font-bold">Please Register</h2>
-                <form   className=" md:w-3/4 lg:w-1/2 mx-auto">
+                <form onSubmit={handleRegistration}  className=" md:w-3/4 lg:w-1/2 mx-auto">
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text font-semibold">Name</span>
