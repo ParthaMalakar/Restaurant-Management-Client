3a1dc8367f782cecb7795f1d9d55fa9ed3f05599
ParthaMalakar <parthamalakar4321@gmail.com>
2023-11-06 23:46:04 +0600

    validation added for quantity in  ordering page

--- src/pages/Ordering/Ordering.jsx
+++ src/pages/Ordering/Ordering.jsx
@@ -1,17 +1,51 @@
 import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import { AuthContext } from '../../provider/Authprovider';
+import Swal from 'sweetalert2';
 
 const Ordering = () => {
     const food = useLoaderData();
-    const { _id, food_name, food_image, food_category, price, quantity } = food;
+    const { _id, food_name, food_image, food_category,made_by, price, quantity } = food;
     const { user } = useContext(AuthContext);
-    console.log(user)
+    const handleOrder = (e) => {
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+
+        const Quantity = form.get('quantity');
+        const BName = form.get('BName')
+        if (BName == made_by ) {
+            Swal.fire({
+                title: 'Can not purchase Own added product!',
+                text: 'Do you want to continue',
+                icon: 'error',
+                confirmButtonText: 'Cool'
+            })
+            return;
+        }
+        if (Quantity > quantity) {
+            Swal.fire({
+                title: 'You Order quantity must be less than available Quantity!',
+                text: 'Do you want to continue',
+                icon: 'error',
+                confirmButtonText: 'Cool'
+            })
+            return;
+        }
+        if (Quantity <= 0) {
+            Swal.fire({
+                title: 'you can not order 0 or less quantity!',
+                text: 'Do you want to continue',
+                icon: 'error',
+                confirmButtonText: 'Cool'
+            })
+            return;
+        }
+    }
     return (
         <div>
             <div className="bg-[#58778f1a]">
                 <h2 className="text-3xl my-7 pt-5 text-center font-bold">Please Give Information for Order</h2>
-                <form className=" md:w-3/4 lg:w-1/2 mx-auto">
+                <form onSubmit={handleOrder} className=" md:w-3/4 lg:w-1/2 mx-auto">
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text font-semibold">Food Name</span>
@@ -28,7 +62,7 @@ const Ordering = () => {
                         <label className="label">
                             <span className="label-text font-semibold">Available Quantity: {quantity}</span>
                         </label>
-                        <input type="text" required  name="quantity" placeholder="Quantity you need" className="input input-bordered" />
+                        <input type="text" required name="quantity" placeholder="Quantity you need" className="input input-bordered" />
                     </div>
                     <div className="form-control">
                         <label className="label">
@@ -56,8 +90,8 @@ const Ordering = () => {
                     </div>
                 </form>
             </div>
-            </div>
-            );
+        </div>
+    );
 };
 
-            export default Ordering;
+export default Ordering;
